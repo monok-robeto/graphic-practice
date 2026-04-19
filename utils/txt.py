edd1e66db38471c9de5b11c39b2128914ac4bf85
monok-robeto <monokrobeto@gmail.com>
2026-04-19 22:49:27 +0700

    setup flow demo

--- utils/txt.py
+++ utils/txt.py
@@ -18,7 +18,7 @@ def label(text, pos, color= color.GREY_0):
     app.surface.blit(_font(default_font, 16).render(text, True, color), pos)
 
 def title(text):
-    app.surface.blit(_font(default_font, 22, bold=True).render(text, True, color.WHITE_0), (20, 20))
+    app.surface.blit(_font(default_font, 22, bold=True).render(text, True, color.WHITE_0), (20, 25))
 
 def hint(text):
     H = app.surface.get_height()
